3264e604266766921c6c2ecc668eb12757b89df2
Ednardo Gomes <ednardo@email.com>
2020-07-14 20:44:58 -0300

    Criando formulário para cadastro de novo professor em create.njk

--- routes.js
+++ routes.js
@@ -2,15 +2,23 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-    return res.render('teachers/index')
+    return res.render("teachers/index")
 })
 
 routes.get('/teachers', function(req, res) {
-    return res.render('teachers/index')
+    return res.render("teachers/index")
+})
+
+routes.get('/teachers/create', function(req, res) {
+    return res.render("teachers/create")
 })
 
 routes.get('/students', function(req, res) {
-    return res.render('students/index')
+    return res.render("students/index")
+})
+
+routes.post('/teachers', function(req, res) {
+    return res.send({"message": "Ok!"})
 })
 
 module.exports = routes
