04817737f6235ff59dfd702c0ba86521ae89d06c
Ednardo Gomes <ednardo@email.com>
2020-07-16 00:30:31 -0300

    Criado arquivo teachers.js e configurada a ação de garar arquivo data.js ao submetar o fomulário.

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
     return res.render("teachers/index")
@@ -13,12 +14,10 @@ routes.get('/teachers/create', function(req, res) {
     return res.render("teachers/create")
 })
 
+routes.post('/teachers', teachers.post)
+
 routes.get('/students', function(req, res) {
     return res.render("students/index")
-})
-
-routes.post('/teachers', function(req, res) {
-    return res.send({"message": "Ok!"})
 })
 
 module.exports = routes
