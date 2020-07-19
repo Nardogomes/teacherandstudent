796baa73ae5d7256aff2112ec0680292c56c157a
Ednardo Gomes <ednardo@email.com>
2020-07-19 16:05:14 -0300

    Ajustando o arquivo data.json para salvar mais dados.

--- teachers.js
+++ teachers.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const data = require('./data.json')
 
 //Create
 exports.post = function(req, res) {
@@ -10,7 +11,12 @@ exports.post = function(req, res) {
             return res.send("Por favor preencha os campos.")
     }
 
-    fs.writeFile("data.json", JSON.stringify(req.body), function(err) {
+    req.body.birth = Date.parse(req.body.birth)
+    req.body.created_at = Date.now()
+
+    data.instructors.push(req.body)
+
+    fs.writeFile("data.json", JSON.stringify(data, null, 2), function(err) {
         if(err) return res.send("Erre na escrita do arquivo.")
 
         return res.redirect("/teachers")
