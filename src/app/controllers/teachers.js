318cfbb6a0afa7b101ef52291c80144f490c49b2
Ednardo Gomes <ednardo@email.com>
2020-08-27 21:37:19 -0300

    Editando dados no banco.

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -1,3 +1,4 @@
+const { age, date } = require('../../lib/utils')
 const Teacher = require('../models/teacher')
 
 module.exports = {
@@ -22,20 +23,38 @@ module.exports = {
         })
     },
     show(req, res) {
-        return
+        Teacher.find(req.params.id, function(teacher) {
+            if(!teacher) return res.send("Professor não encontrado.")
+
+            teacher.age = age(teacher.birth)
+            teacher.disciplinas = teacher.disciplinas.split(",")
+
+            teacher.created_at = date(teacher.created_at).format
+
+            return res.render("teachers/show", { teacher })
+        })
     },
     edit(req, res) {
-        return
+        Teacher.find(req.params.id, function(teacher) {
+            if(!teacher) return res.send("Professor não encontrado.")
+
+            teacher.birth = date(teacher.birth).iso
+
+            return res.render("teachers/edit", { teacher })
+        })
     },
     put(req, res) {
         const keys = Object.keys(req.body)
 
         for(key of keys) {
-            if(req.body[key] == "")
+            if(req.body[key] == "") {
                 return res.send("Por favor preencha os campos.")
+            }
         }
 
-        return
+        Teacher.update(req.body, function() {
+            return res.redirect(`/teachers/${req.body.id}`)
+        })
     },
     delete(req, res) {
         return
